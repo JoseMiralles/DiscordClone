52d773eb304264e2a63beac0dc31e0a750862a34
Jose Miralles <josemiralles1991@gmail.com>
2021-05-04 18:11:33 -0400

    finished navbar collapsible menu

--- frontend/src/Components/NavBar.tsx
+++ frontend/src/Components/NavBar.tsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { logout } from "../Actions/SessionActions";
 import { IAppSate } from "../store";
+import { useState } from "react";
 
 interface Props {
     isAuthenticated: boolean
@@ -12,17 +13,25 @@ const NavBar = ({ isAuthenticated }: Props) => {
 
     const dispatch = useDispatch();
 
+    const [menuClosed, setMenuClosed] = useState(true);
+
+    const flipMenu = () => setMenuClosed(!menuClosed);
+    const itemSelected = () => setMenuClosed(true);
+
+    const menuClass = menuClosed ? "hidden" : "";
+    const icon = menuClosed ? "menu" : "close";
+
     return (
         <nav className="section">
             <div className="container" id="nav-bar">
                 <div>
                     <div id="nav-logo" className="logo">InTalk</div>
                 </div>
-                <div id="nav-menu-button">
-                    <div className="material-icons">menu</div>
+                <div onClick={flipMenu} id="nav-menu-button">
+                    <div className="material-icons">{ icon }</div>
                 </div>
             </div>
-            <div id="nav-menu-wrapper" className="container">
+            <div onClick={itemSelected} id="nav-menu-wrapper" className={`container ${menuClass}`}>
                 <ul id="nav-menu">
                     {!isAuthenticated
                         ? <>
