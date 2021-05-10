ad4623fb58cf7740ba6585a9b591e3872d984cd9
Jose Miralles <josemiralles1991@gmail.com>
2021-05-10 15:36:10 -0400

    JWT is now retained if it has not yet expired when a session is being reset.

--- frontend/src/Util/SessionUtil.ts
+++ frontend/src/Util/SessionUtil.ts
@@ -42,7 +42,7 @@ export const refreshAccessToken = async (): Promise<string> => {
         return res.data.token;
     } catch (error) {
         // Retain session if the jwt has not yet expired.
-        const isUnexpired = error.response.data.errors.find((e: string) => e.includes("UNEXPIRED_TOKEN"));
+        const isUnexpired = error.response?.data.errors.find((e: string) => e.includes("UNEXPIRED_TOKEN"));
         if (isUnexpired) {
             updateTokens({token: tokenSet.jwt, refreshToken: tokenSet.refreshToken});
             return tokenSet.jwt;
@@ -51,6 +51,36 @@ export const refreshAccessToken = async (): Promise<string> => {
         throw error;
     }
 };
+
+/**
+ * Sets up middleware that automatically refreshes tokens when needed.
+ * @param onRefreshFailed A callback that gets called when a token refresh fails.
+ */
+export function setupTokenRefresh(onRefreshFailed: (error: any) => void) {
+    axios.interceptors.response.use(
+        response => response,
+        async (error) => {
+            // Request was not successful.
+            const originalRequest = error.config;
+            if ([401, 403].includes(error.response?.status) && !originalRequest._retry) {
+                originalRequest._retry = true;
+                await refreshAccessToken();
+                return axios(originalRequest);
+            }
+            
+            /**
+             * At this point, the token refresh failed. So check if it failed because the JWT still
+             * has not yet expired. Which means that there is no need to refresh.
+             */ 
+            if (error.response.data.errors.find((e: string) => e.includes("UNEXPIRED_TOKEN"))) {
+                // Return the tokens that are stored locally.
+                const tokens = getTokenSet();
+                return { data: { token: tokens.jwt, refreshToken: tokens.refreshToken } };
+            }
+            onRefreshFailed(error);
+        }
+    );
+}
 
 /**
  * Called when tokens are refreshed, or the user is authenticated.
@@ -93,20 +123,4 @@ export const decodeUser = (jwt: string): IUser => {
         id: decoded.nameid
     };
 };
-
-export function setupTokenRefresh(onRefreshFailed: (error: any) => void) {
-    axios.interceptors.response.use(
-        response => response,
-        async (error) => {
-            // Request was not successful.
-            const originalRequest = error.config;
-            if ([401, 403].includes(error.response?.status) && !originalRequest._retry) {
-                originalRequest._retry = true;
-                await refreshAccessToken();
-                return axios(originalRequest);
-            }
-            onRefreshFailed(error);
-        }
-    );
-}
 
