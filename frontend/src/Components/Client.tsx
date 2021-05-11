9a8c0b13aeb84063afc41422f719b6b703bad21f
Jose Miralles <josemiralles1991@gmail.com>
2021-05-11 01:26:53 -0400

    Made signalr connection a part of state.

--- frontend/src/Components/Client.tsx
+++ frontend/src/Components/Client.tsx
@@ -4,29 +4,34 @@ import ServerBar from "./Servers/ServerBar"
 import UserBar from "./Users/UserBar";
 import * as SignalR from "@microsoft/signalr";
 import { getTokenSet } from "../Util/SessionUtil";
+import { useEffect, useState } from "react";
 
 const Client = () => {
 
     const selected = useSelector((s: AppState) => s.servers.selected);
+    const [connection, setConnection] = useState<SignalR.HubConnection | null>(null);
 
-    // Add this to a custom hook.
-    const connection = new SignalR.HubConnectionBuilder()
-        .withUrl("https://localhost:5001/hubs/intalk",
-            { accessTokenFactory: () => getTokenSet().jwt })
-        .build();
-    
-    connection.on("messageReceived", (message: string) => {
-        console.log(message);
-    });
+    useEffect(() => {
+        // TODO: Add this to a custom hook.
+        const c = new SignalR.HubConnectionBuilder()
+            .withUrl("https://localhost:5001/hubs/intalk",
+                { accessTokenFactory: () => getTokenSet().jwt })
+            .build();
 
-    connection.start().then(() => {
-        connection.send("ChangeRole", "hi!");
-    });
+        c.on("messageReceived", (message: string) => {
+            console.log(message);
+        });
+
+        c.start().then(() => {
+            c.send("ChangeRole", "hi!");
+            setConnection(c);
+        });
+    }, []);
 
     return (
         <section id="client-section">
             <ServerBar />
-            { selected !== null && <UserBar/> }
+            { selected !== null && <UserBar />}
         </section>
     );
 };
